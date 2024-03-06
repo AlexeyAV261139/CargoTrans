b80e0878cf0f6659d7387c818cce7c7598884df7
Maria <mashadrid@icloud.com>
2024-03-06 14:28:05 +0300

    Добавление данных 1

--- DB/Entityes/CargoType.cs
+++ DB/Entityes/CargoType.cs
@@ -7,7 +7,7 @@ public partial class CargoType
 {
     public int Id { get; set; }
 
-    public string? Name { get; set; }
+    public string? Size { get; set; }
 
     public virtual ICollection<Cargo> Cargos { get; set; } = new List<Cargo>();
 }
--- DB/Entityes/Driver.cs
+++ DB/Entityes/Driver.cs
@@ -11,7 +11,7 @@ public partial class Driver
 
     public string LastName { get; set; } = null!;
 
-    public int Phone { get; set; }
+    public string Phone { get; set; }
 
     public virtual ICollection<ActiveRoute> ActiveRoutes { get; set; } = new List<ActiveRoute>();
 }
