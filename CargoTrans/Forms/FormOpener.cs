b81b95a45485db33ce865a48d5485cebacbe79e6
Alexey <Alexey261139@mail.ru>
2024-03-26 16:36:01 +0300

    класс открытия форм перенесен в отдельный файл

---
+++ CargoTrans/Forms/FormOpener.cs
@@ -0,0 +1,18 @@
+﻿using View.Forms;
+
+namespace CargoTrans
+{
+    public class FormOpener
+    {
+        public void AddCargo() => new CargoAddForm().Show();
+
+        public void AddCar() => new CarAddForm().Show();
+
+        public void AddRoute() => new RouteAddForm().Show();
+
+        public void AddDriver() => new DriverAddForm().Show();
+
+        public void AddActiveRoute() => new ActiveRouteAddForm().Show();
+
+    }
+}
