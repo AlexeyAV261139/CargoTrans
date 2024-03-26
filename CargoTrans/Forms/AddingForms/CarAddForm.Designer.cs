b12d44c6d4dcd5f4e5efdd688124a1f065ca0e42
Alexey <Alexey261139@mail.ru>
2024-03-26 23:07:39 +0300

    На форму добавления авто добавлены и настроены элементы

--- CargoTrans/Forms/AddingForms/CarAddForm.Designer.cs
+++ CargoTrans/Forms/AddingForms/CarAddForm.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CarAddForm";
+            capacityTextBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            appendButton = new Button();
+            numberMaskedTextBox = new MaskedTextBox();
+            brandMaskedTextBox = new MaskedTextBox();
+            SuspendLayout();
+            // 
+            // capacityTextBox
+            // 
+            capacityTextBox.Location = new Point(162, 148);
+            capacityTextBox.Name = "capacityTextBox";
+            capacityTextBox.Size = new Size(162, 23);
+            capacityTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Марка";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Номер";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Вместимость в кг.";
+            // 
+            // appendButton
+            // 
+            appendButton.Location = new Point(135, 202);
+            appendButton.Name = "appendButton";
+            appendButton.Size = new Size(75, 23);
+            appendButton.TabIndex = 6;
+            appendButton.Text = "Добавить";
+            appendButton.UseVisualStyleBackColor = true;
+            // 
+            // numberMaskedTextBox
+            // 
+            numberMaskedTextBox.Location = new Point(162, 109);
+            numberMaskedTextBox.Mask = "(999) 000-0000";
+            numberMaskedTextBox.Name = "numberMaskedTextBox";
+            numberMaskedTextBox.Size = new Size(162, 23);
+            numberMaskedTextBox.TabIndex = 7;
+            // 
+            // brandMaskedTextBox
+            // 
+            brandMaskedTextBox.Location = new Point(162, 67);
+            brandMaskedTextBox.Mask = "L_000_LL_00";
+            brandMaskedTextBox.Name = "brandMaskedTextBox";
+            brandMaskedTextBox.Size = new Size(162, 23);
+            brandMaskedTextBox.TabIndex = 8;
+            brandMaskedTextBox.ValidatingType = typeof(int);
+            // 
+            // CarAddForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(380, 261);
+            Controls.Add(brandMaskedTextBox);
+            Controls.Add(numberMaskedTextBox);
+            Controls.Add(appendButton);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(capacityTextBox);
+            Name = "CarAddForm";
+            Text = "Добавление авто";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox capacityTextBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button appendButton;
+        private MaskedTextBox numberMaskedTextBox;
+        private MaskedTextBox brandMaskedTextBox;
     }
 }
--- CargoTrans/Forms/AddingForms/CarAddForm.cs
+++ CargoTrans/Forms/AddingForms/CarAddForm.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace View.Forms
+﻿namespace View.Forms
 {
     public partial class CarAddForm : Form
     {
         public CarAddForm()
         {
             InitializeComponent();
+
         }
     }
 }
