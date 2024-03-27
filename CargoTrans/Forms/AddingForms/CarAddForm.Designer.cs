fe69b27ad3ea74f159ea3049cee95a05f466433a
Alexey <Alexey@DESKTOP-H2N279O>
2024-03-27 11:02:48 +0300

    Реализована форма добавления авто

--- CargoTrans/Forms/AddingForms/CarAddForm.Designer.cs
+++ CargoTrans/Forms/AddingForms/CarAddForm.Designer.cs
@@ -34,81 +34,86 @@
             label3 = new Label();
             appendButton = new Button();
             numberMaskedTextBox = new MaskedTextBox();
-            brandMaskedTextBox = new MaskedTextBox();
+            brandTextBox = new TextBox();
             SuspendLayout();
             // 
             // capacityTextBox
             // 
-            capacityTextBox.Location = new Point(162, 148);
+            capacityTextBox.Location = new Point(185, 197);
+            capacityTextBox.Margin = new Padding(3, 4, 3, 4);
             capacityTextBox.Name = "capacityTextBox";
-            capacityTextBox.Size = new Size(162, 23);
+            capacityTextBox.Size = new Size(185, 27);
             capacityTextBox.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(40, 70);
+            label1.Location = new Point(46, 93);
             label1.Name = "label1";
-            label1.Size = new Size(43, 15);
+            label1.Size = new Size(54, 20);
             label1.TabIndex = 3;
             label1.Text = "Марка";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(40, 112);
+            label2.Location = new Point(46, 149);
             label2.Name = "label2";
-            label2.Size = new Size(45, 15);
+            label2.Size = new Size(57, 20);
             label2.TabIndex = 4;
             label2.Text = "Номер";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(40, 151);
+            label3.Location = new Point(46, 201);
             label3.Name = "label3";
-            label3.Size = new Size(106, 15);
+            label3.Size = new Size(132, 20);
             label3.TabIndex = 5;
             label3.Text = "Вместимость в кг.";
             // 
             // appendButton
             // 
-            appendButton.Location = new Point(135, 202);
+            appendButton.Location = new Point(154, 269);
+            appendButton.Margin = new Padding(3, 4, 3, 4);
             appendButton.Name = "appendButton";
-            appendButton.Size = new Size(75, 23);
+            appendButton.Size = new Size(86, 31);
             appendButton.TabIndex = 6;
             appendButton.Text = "Добавить";
             appendButton.UseVisualStyleBackColor = true;
+            appendButton.Click += appendButton_Click;
             // 
             // numberMaskedTextBox
             // 
-            numberMaskedTextBox.Location = new Point(162, 109);
-            numberMaskedTextBox.Mask = "(999) 000-0000";
+            numberMaskedTextBox.Location = new Point(185, 146);
+            numberMaskedTextBox.Margin = new Padding(3, 4, 3, 4);
+            numberMaskedTextBox.Mask = "L_000_LL_00";
             numberMaskedTextBox.Name = "numberMaskedTextBox";
-            numberMaskedTextBox.Size = new Size(162, 23);
-            numberMaskedTextBox.TabIndex = 7;
+            numberMaskedTextBox.Size = new Size(185, 27);
+            numberMaskedTextBox.TabIndex = 8;
+            numberMaskedTextBox.ValidatingType = typeof(int);
             // 
-            // brandMaskedTextBox
+            // brandTextBox
             // 
-            brandMaskedTextBox.Location = new Point(162, 67);
-            brandMaskedTextBox.Mask = "L_000_LL_00";
-            brandMaskedTextBox.Name = "brandMaskedTextBox";
-            brandMaskedTextBox.Size = new Size(162, 23);
-            brandMaskedTextBox.TabIndex = 8;
-            brandMaskedTextBox.ValidatingType = typeof(int);
+            brandTextBox.Location = new Point(185, 93);
+            brandTextBox.Margin = new Padding(3, 4, 3, 4);
+            brandTextBox.Name = "brandTextBox";
+            brandTextBox.Size = new Size(185, 27);
+            brandTextBox.TabIndex = 9;
             // 
             // CarAddForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(380, 261);
-            Controls.Add(brandMaskedTextBox);
+            ClientSize = new Size(434, 348);
+            Controls.Add(brandTextBox);
             Controls.Add(numberMaskedTextBox);
             Controls.Add(appendButton);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(capacityTextBox);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "CarAddForm";
             Text = "Добавление авто";
             ResumeLayout(false);
@@ -123,6 +128,6 @@
         private Label label3;
         private Button appendButton;
         private MaskedTextBox numberMaskedTextBox;
-        private MaskedTextBox brandMaskedTextBox;
+        private TextBox brandTextBox;
     }
 }
--- CargoTrans/Forms/AddingForms/CarAddForm.cs
+++ CargoTrans/Forms/AddingForms/CarAddForm.cs
@@ -1,11 +1,46 @@
-﻿namespace View.Forms
+﻿using Application.Interfaces.Services;
+using Core.Models;
+
+namespace View.Forms
 {
     public partial class CarAddForm : Form
     {
-        public CarAddForm()
+        private readonly CarService _service;
+
+        public CarAddForm(CarService service)
         {
             InitializeComponent();
+            _service = service;
+        }
 
+        private async void appendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var car = GetCarFromForm();
+                await _service.CreateCar(car);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Успешно!");
+            }
+        }
+
+        private Car GetCarFromForm()
+        {
+            if (!int.TryParse(capacityTextBox.Text, out var capacity) || capacity < 0)
+                throw new Exception("некорректная вместимость");
+
+            return new Car
+            {
+                Brand = brandTextBox.Text,
+                Number = numberMaskedTextBox.Text,
+                LoadCapacityPerKg = capacity
+            };
         }
     }
 }
